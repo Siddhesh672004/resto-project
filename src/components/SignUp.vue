5a015880afc0d9b7b9d7d1a94ade0521c99b9f96
Siddhesh Chaudhari <siddheshavic67@gmail.com>
2025-06-15 20:05:26 +0530

    Login Page Design and Routing Done

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -8,6 +8,9 @@
         <input type="password" v-model="password" placeholder="Enter Password" />
         <button v-on:click="signUp">Sign Up</button>
 
+        <p>
+            <router-link to="/login-page">Login</router-link>
+        </p>
     </div>
 </template>
 
@@ -56,29 +59,6 @@ export default {
 
 <style>
 
-.logo{
-    width: 100px;
-}
-
-.register input {
-    width: 300px;
-    height: 40px;
-    padding-left: 20px;
-    display: block;
-    margin-bottom: 30px;
-    margin-right: auto;
-    margin-left: auto;
-    border: 1px solid skyblue;
-}
-
-.register button {
-    width: 320px;
-    height: 40px;
-    border: 1px solid skyblue;
-    background: skyblue;
-    color: #fff;
-    cursor: pointer;
-}
 
 </style>
 
