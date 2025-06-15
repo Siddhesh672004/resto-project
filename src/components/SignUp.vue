8eed361e035bd53899e02ef5e25fb4a8bc5d0e6a
Siddhesh Chaudhari <siddheshavic67@gmail.com>
2025-06-15 19:34:31 +0530

    Set-up Routing and HomePage component

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -37,6 +37,7 @@ export default {
             console.warn(result);
             if(result.status == 201){
                 localStorage.setItem("user-info",JSON.stringify(result.data))
+                this.$router.push({name:'HomePage'})
             }
         }
     }
